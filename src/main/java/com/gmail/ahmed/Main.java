dada44f217c37e2f68d1c4ec04893c6a24f75f1a
Ahmed Slimani <ahmed.slimani@soprasteria.com>
2023-12-09 18:29:40 +0100

    partie 2

--- src/main/java/com/gmail/ahmed/Main.java
+++ src/main/java/com/gmail/ahmed/Main.java
@@ -3,6 +3,7 @@ package com.gmail.ahmed;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,8 +13,7 @@ public class Main {
             String ligne;
             int somme = 0;
             while ((ligne = lecteur.readLine()) != null) {
-                //System.out.println(ligne);
-                somme +=  getFirstLast(ligne);
+                somme +=  getFirstLastFromLetter(ligne);
             }
             System.out.println(somme);
         } catch (IOException e) {
@@ -21,15 +21,64 @@ public class Main {
         }
     }
 
-    private static int getFirstLast(String ligne) {
-        char[] digits = new char[ligne.toCharArray().length];
-        int idnx = 0;
-        for (char caractere : ligne.toCharArray()) {
-            if (Character.isDigit(caractere)){
-                digits[idnx] = caractere;
-                idnx++;
+    private static int getFirstLastFromLetter(String ligne) {
+        List<String> digitsLitters =
+                Arrays.asList("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+        for (String digitLetter: digitsLitters) {
+            int indiceMap = ligne.indexOf(digitLetter);
+            while (indiceMap != -1) {
+                treeMap.put(indiceMap, digitLetter);
+                indiceMap = ligne.indexOf(digitLetter, indiceMap +1);
             }
         }
-        return Integer.parseInt(String.format("%s%s", digits[0], digits[idnx - 1]));
+
+        int indxForFirst = treeMap.isEmpty() ? ligne.length() : treeMap.firstKey();
+        String first = extractDigitFromFirst(ligne, indxForFirst);
+        if (first == null){
+            first = mappingDigit(treeMap.firstEntry().getValue());
+        }
+
+        int idnxForLast = treeMap.isEmpty() ? 0 : treeMap.lastEntry().getKey() + treeMap.lastEntry().getValue().length();
+        String last = extractDigitFromLast(ligne, idnxForLast);
+        if (last == null) {
+            last = mappingDigit(treeMap.lastEntry().getValue());
+        }
+
+        return Integer.parseInt(first + last);
+    }
+    private static String extractDigitFromFirst(String ligne, int indxForFirst) {
+        char[] digits = ligne.toCharArray();
+        for (int i = 0; i < indxForFirst ; i++) {
+            if (Character.isDigit(digits[i])){
+                return String.valueOf(digits[i]);
+            }
+        }
+        return null;
+    }
+
+    private static String extractDigitFromLast(String ligne, int idnxForLast) {
+        char[] digits = ligne.toCharArray();
+        for (int i = ligne.length() - 1; i >= idnxForLast ; i--) {
+            if (Character.isDigit(digits[i])){
+                return String.valueOf(digits[i]);
+            }
+        }
+        return null;
+    }
+    private static String mappingDigit(String letters) {
+        Map<String, String> map = new HashMap<>();
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+
+        return map.get(letters);
     }
 }
